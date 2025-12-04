1e942278644a83e30a17a7560797b474d4b55da1
ethan <ethaneshom@gmail.com>
2025-12-04 11:50:02 -0800

    added translation for block of cheese. registered block of cheese.

--- src/main/java/net/ethan/cheese/block/ModBlocks.java
+++ src/main/java/net/ethan/cheese/block/ModBlocks.java
@@ -1,14 +1,21 @@
 package net.ethan.cheese.block;
 
 import net.ethan.cheese.Cheese;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
+
+    public static final Block BLOCK_OF_CHEESE = registerBlock("block_of_cheese",
+            new Block(AbstractBlock.Settings.create().strength(0.7F).sounds(BlockSoundGroup.SLIME)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItems(name, block);
@@ -22,6 +29,10 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         Cheese.LOGGER.info("Registering mod blocks for " + Cheese.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ModBlocks.BLOCK_OF_CHEESE);
+        });
     }
 
 }

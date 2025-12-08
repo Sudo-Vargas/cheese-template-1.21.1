349618b2b51fc14b1103f690b77ea840cea5f543
ethan <ethaneshom@gmail.com>
2025-12-07 18:01:20 -0800

    converted recipes to use cheddar cheese instead of old generic cheese. removed generic cheese code.

--- src/main/java/net/ethan/cheese/item/ModItems.java
+++ src/main/java/net/ethan/cheese/item/ModItems.java
@@ -3,6 +3,8 @@ package net.ethan.cheese.item;
 import net.ethan.cheese.Cheese;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.component.type.FoodComponent;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -10,6 +12,14 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+
+    public static final Item MOLDY_CHEESE_WEDGE = registerItem("moldy_cheese_wedge", new Item(new Item.Settings()
+            .food(new FoodComponent.Builder()
+                    .nutrition(1)
+                    .saturationModifier(0.1f)
+                    .snack()
+                    .statusEffect(new StatusEffectInstance(StatusEffects.NAUSEA,200), 1.0f)
+                    .build())));
 
     public static final Item CHEDDAR_CHEESE_WEDGE = registerItem("cheddar_cheese_wedge", new Item(new Item.Settings()
             .food(new FoodComponent.Builder()
@@ -27,8 +37,8 @@ public class ModItems {
         Cheese.LOGGER.info("Registering mod items for " + Cheese.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries ->{
-
             entries.add(CHEDDAR_CHEESE_WEDGE);
+            entries.add(MOLDY_CHEESE_WEDGE);
         });
     }
 }
